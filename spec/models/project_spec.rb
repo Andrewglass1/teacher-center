f8e2fc25856fc4b7948e5eb3353fde72241228bb
Andrewglass1 <Andrewglass1@gmail.com>
2012-07-11 17:41:44 -0400

    tests work

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,18 +2,18 @@ require 'spec_helper'
 
 describe Project do
   context ".create_by_project_url" do
-    let(:project_reponse) {
+    let(:project_response) {
       {
         'city' => 'blah'
       }
     }
-    before do
-      stub(:get_project).and_return(project_response)
+    before :each do
+      Project.any_instance.stub(:get_project).and_return(project_response)
     end
 
     context "when given a valid project url" do
       it "creates a project with all valid attributes" do
-        expect { Project.get_project('http://www.donorschoose.org/project/biotechnology-applications/816888/') }.to change(Project.count).by(1)
+        expect { Project.create_by_project_url('http://www.donorschoose.org/project/biotechnology-applications/816888/') }.to change{Project.count}.by(1)
       end
     end
   end
