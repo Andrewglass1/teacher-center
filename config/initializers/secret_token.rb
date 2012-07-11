3939fbd3296a821a816b0153ed340e9b24febe59
Andrewglass1 <Andrewglass1@gmail.com>
2012-07-11 13:31:07 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teachercenter::Application.config.secret_token = '858e8a69b6019f9b30b4f13b9e9b0010eccc72bd614a05909026ad0846f097303f4006ec2ef9be0fc25579aebbecad577b598e3f05ef45e75be4b408682e4fa5'
