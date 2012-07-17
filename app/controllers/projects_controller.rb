fa4b58458eec46792c596f0abee64e6073832339
Mary Cutrali <mary.cutrali@gmail.com>
2012-07-17 14:57:55 -0400

    style dashboard

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,11 @@
 class ProjectsController < ApplicationController
 
+  def create 
+    @project = Project.new 
+    project.save
+  end 
+
+
   def show 
     @project = Project.find_by_id(params[:id])
   end 
