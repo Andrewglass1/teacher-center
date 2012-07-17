04142491b735f32da7ed6608c5d3df50a01f959d
Andrewglass1 <Andrewglass1@gmail.com>
2012-07-17 16:01:34 -0400

    Create project when inputting url on welcome page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,4 +3,13 @@ class ProjectsController < ApplicationController
   def show 
     @project = Project.find_by_id(params[:id])
   end 
+
+  def create
+    @project = Project.create_by_project_url(params["project_url"])
+    if @project
+      redirect_to project_path(@project)
+    else
+      redirect_to root_path, :notice => "Please input a valid project url"
+    end
+  end
 end
