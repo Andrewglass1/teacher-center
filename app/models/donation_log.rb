a257761bbacb38d764cf537b6326fe9e31e75e2f
Andrewglass1 <Andrewglass1@gmail.com>
2012-07-18 18:02:50 -0400

    Calculate donations today, over range of dates

--- app/models/donation_log.rb
+++ app/models/donation_log.rb
@@ -1,4 +1,14 @@
 class DonationLog < ActiveRecord::Base
-  attr_accessible :date, :amount_funded_cents
+  attr_accessible :date, :amount_funded_cents, :project_id
   belongs_to :project
+
+
+  def self.donations_today_cents(project_id)
+    DonationLog.where(:date => Date.today, :project_id => project_id).first.amount_funded_cents
+  end
+
+  def self.donations_over_days_cents(project_id, days)
+    donations = DonationLog.where(:date => Date.today-days..Date.today, :project_id => project_id).all
+    donations.sum(&:amount_funded_cents)
+  end
 end
