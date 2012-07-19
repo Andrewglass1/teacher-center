e195bc8a4e9182d954e774572090d38fee6c0277
athal7 <andrewthal88@gmail.com>
2012-07-19 17:42:19 -0400

    Fix seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,11 @@ ProjectTask.destroy_all
 
 u = User.create(email: "MikeSilvis@gmail.com", password: "hungry")
 
-t1 = Task.create(medium: "Twitter"      , stage: "Phase 1", description: "Check out my donors choose project")
-t2 = Task.create(medium: "Facebook"     , stage: "Phase 2", description: "Check out my donors choose project")
-t3 = Task.create(medium: "Mail"         , stage: "Phase 3", description: "Check out my donors choose project")
-t4 = Task.create(medium: "PrintAndShare", stage: "Phase 3", description: "Check out my donors choose project")
-t4 = Task.create(medium: "Email"        , stage: "Phase 3", description: "Check out my donors choose project")
+t1 = Task.create(medium: "Twitter"      , stage: "Phase 1")
+t2 = Task.create(medium: "Facebook"     , stage: "Phase 2")
+t3 = Task.create(medium: "Mail"         , stage: "Phase 3")
+t4 = Task.create(medium: "PrintAndShare", stage: "Phase 3")
+t4 = Task.create(medium: "Email"        , stage: "Phase 3")
 
 p = Project.create_by_project_url("812882")
 u.projects << [p]
