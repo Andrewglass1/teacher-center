29973a621fc9841d2fd6d7c68a51787facba9377
athal7 <andrewthal88@gmail.com>
2012-07-23 16:11:14 -0400

    Install high charts

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
-  before_filter :authenticate_user!, only: [:show]
-  before_filter :find_project, :confirm_your_project, only: [:show]
+  before_filter :authenticate_user!, :find_project, :confirm_your_project, 
+    :create_charts, only: [:show]
 
   def show
   end
@@ -9,13 +9,13 @@ class ProjectsController < ApplicationController
     if current_user
       @project = current_user.projects.create_by_project_url(
         params[:project_url])
-      redirect_to project_path(@project)
+        redirect_to project_path(@project)
     else
       create_project_for_guest(params)
     end
   end
 
-private
+  private
 
   def create_project_for_guest(params)
     Project.create_in_thread(params[:project_url])
@@ -31,6 +31,14 @@ private
   def confirm_your_project
     unless @project.user == current_user
       redirect_to root_path, notice: "That is not your project"
+    end
+  end
+
+  def create_charts
+    @donations_charts = LazyHighCharts::HighChart.new('graph') do |f|
+      f.options[:chart][:defaultSeriesType] = "area"
+      f.series(:name=>'John', :data=>[3, 20, 3, 5, 4, 10, 12 ,3, 5,6,7,7,80,9,9])
+      f.series(:name=>'Jane', :data=> [1, 3, 4, 3, 3, 5, 4,-46,7,8,8,9,9,0,0,9] )
     end
   end
 
