3e148c76891dbf8925d03b0fba4f2f215546a843
athal7 <andrewthal88@gmail.com>
2012-07-23 14:15:31 -0400

    Remove project and user from seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,13 +3,8 @@ Task.destroy_all
 Project.destroy_all
 ProjectTask.destroy_all
 
-u = User.create(email: "MikeSilvis@gmail.com", password: "hungry")
-
 t1 = Task.create(medium: "Twitter"      , stage: "Phase 1")
 t2 = Task.create(medium: "Facebook"     , stage: "Phase 2")
 t3 = Task.create(medium: "Mail"         , stage: "Phase 3")
 t4 = Task.create(medium: "Email"        , stage: "Phase 3")
 t4 = Task.create(medium: "PrintAndShare", stage: "Phase 3")
-
-p = Project.create_by_project_url("812882")
-u.projects << [p]
