2c0ad43bd1447b4f785dad23cdea8578d1159f7f
athal7 <andrewthal88@gmail.com>
2012-07-25 14:37:30 -0400

    Refactor ApplicationController#after_sign_in_path_for closes #28

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def after_sign_in_path_for(user)
-    if (request.referer == sign_in_url) && current_user.projects.any?
+    if just_signed_in_and_have_project
       project_path(current_user.projects.last)
-    elsif (request.referer == sign_in_url) || (request.referer == sign_up_url)
+    elsif just_signed_in_or_up
       super
     else
       request.referer || stored_location_for(user) || root_path
@@ -12,6 +12,14 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
+  def just_signed_in_and_have_project
+    (request.referer == sign_in_url) && current_user.projects.any?
+  end
+
+  def just_signed_in_or_up
+    (request.referer == sign_in_url) || (request.referer == sign_up_url)
+  end
 
   def sign_in_url
     url_for(
